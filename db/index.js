237d8d869d9cf48bd9b5eb2be0add715452892b9
ischekoldin <lesterfrance@gmail.com>
2020-08-31 18:25:50 +0500

    changed db settings

--- db/index.js
+++ db/index.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool ({
-    user: "postgres",
-    password: "abcde567",
-    host: "localhost",
+    user: "ybwlkprwfidvcx",
+    password: "7f91bdb938d2c95b9d1c48347ba2a186c959d46db07e91ebfd2be66569215f81",
+    host: "ec2-3-217-87-84.compute-1.amazonaws.com",
     port: 5432,
-    database: "unnotate"
+    database: "dbq657v46ql1ge"
 });
 
 module.exports = pool;
