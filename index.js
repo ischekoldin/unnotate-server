a5d01a5552ab8cddaa340927e967e271f4073279
ischekoldin <lesterfrance@gmail.com>
2020-08-31 20:52:20 +0500

    trying to fix heroku crash on start

--- index.js
+++ index.js
@@ -234,7 +234,7 @@ function generateAccessToken(user) {
 }
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`The server is running on port ${PORT}`);
 });
