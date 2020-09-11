c00a46cddade5b14361b71cab3746d68bd1dbb02
ischekoldin <lesterfrance@gmail.com>
2020-09-11 06:43:49 +0500

    adjusted media queries for mobile devices

--- index.js
+++ index.js
@@ -113,8 +113,8 @@ app.post ("/login", async (req, res) => {
 
                let date = new Date();
                date.setTime(date.setTime(date.getTime()+(14*24*60*60*1000)));
-               res.cookie('unnotateRememberMe', rememberMe, { expires: date, httpOnly: false, sameSite: "lax", domain: "unnotate-client.herokuapp.com" });
-               res.cookie('refreshToken', refreshToken, { expires: date, httpOnly: true, sameSite: "lax", domain: "herokuapp.com" });
+               res.cookie('unnotateRememberMe', rememberMe, { expires: date, httpOnly: false, sameSite: "lax" });
+               res.cookie('refreshToken', refreshToken, { expires: date, httpOnly: true, sameSite: "lax" });
                res.json({ accessToken: accessToken, refreshToken: refreshToken });
 
             } else {
